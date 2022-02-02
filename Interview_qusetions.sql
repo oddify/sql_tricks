fd0e181ca411d928455164fe08741b0111bdeced
oddify <52467386+oddify@users.noreply.github.com>
2022-02-02 10:43:05 +0530

    Theory questions

--- Interview_qusetions.sql
+++ Interview_qusetions.sql
@@ -69,3 +69,49 @@ FROM
     JOIN yearly_sales ys ON p.product_id = ys.product_id)
     where  rn =1
     order by product_id;
+    
+    
+    /* 
+        1.What is basic structure of plsql block?
+        Declare
+        Begin
+        Exception
+        END;
+        
+        
+        2.What is Exception handling?
+            Exception handling in PL/SQL can be adjusted. When an error occurs, the program’s error handling code is included. There are three different sorts of exceptions:
+
+            Pre-defined exceptions are frequent errors that have already been defined. NO DATA FOUND is an example.
+            Undefined exceptions are errors that don’t have a name assigned to them.
+            User-defined exceptions are handled by the user’s own code.
+        3.Mention a few predefined exceptions?
+            The following are some examples of predefined exceptions:
+
+                NO DATA FOUND: A single-row SELECT statement that returns no data.
+                TOO MANY ROWS: A single row SELECT statement that returns many rows.
+                INVALID CURSOR: An incorrect cursor operation was performed.
+                ZERO DIVIDE: Attempt at zero division.
+        4.What is a left outer join? example?
+            In an outer join, unmatched rows in one or both tables can be returned. LEFT JOIN returns only unmatched rows on left table
+            
+        5.What are the types of parameters in PL/SQL?
+        There are three types of parameters in PL/SQL which are as follows:
+
+            IN: IN parameters allow you to send values to the procedure that is being called, and they can be set to default values. They behave as constants and cannot be changed.
+            OUT: OUT arguments must be mentioned because they return a value to the caller. These are uninitialized variables that can’t be used in expressions.
+            IN OUT: IN OUT parameters send starting values to a procedure and then return updated values to the caller. These parameters should be treated as initialized variables and given a value.
+    
+        6.Why do we use Index in a table?
+            In a table, we use indexes to allow quick access to rows. For procedures that return a small percentage of a table’s rows, indexes allow faster access to data.
+            
+         7.Which cursor attributes are the result of a DML statement saved when it is executed?
+            The statement’s result is saved in four cursor attributes. The four attributes are: 
+
+            SQL% FOUND 
+            SQL% NOTFOUND 
+            SQL% ROWCOUNT 
+            SQL% ISOPEN
+            
+          
+    */
